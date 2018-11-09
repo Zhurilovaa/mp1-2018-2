0be02f4ff4e785637696d3b5e41b49f6d1c9b704
Журилова Анастасия Сергеевна <zhurilova.a@ccam.cmc.unn.net>
2018-11-09 10:33:33 +0300

    Task5 начало

---
+++ Zhurilova.a/task5/Source5.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <locale.h>
+#include <stdlib.h>
+#include<string.h>
+#include<memory.h>
+#include<stdio.h>
+#include<math.h>
+main()
+{
+	setlocale(LC_ALL, "Rus");
+	
+}
