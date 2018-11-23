9ed9d210401018d5acfb4205b7a4b9f74bfab153
Журилова Анастасия Сергеевна <zhurilova.a@ccam.cmc.unn.net>
2018-11-23 10:39:39 +0300

    Незаконченное task5

--- Zhurilova.a/task5/Source5.cpp
+++ Zhurilova.a/task5/Source5.cpp
@@ -2,9 +2,61 @@
 #include <locale.h>
 #include <stdlib.h>
 #include<string.h>
-#include<memory.h>
 #include<stdio.h>
-#include<math.h>
+#include<time.h>
+//структура для файлов
+struct dlfail
+{
+	char name[50];
+	unsigned long long size;
+};
+int compare(struct dlfail f, struct dlfail l, int s)//сравнение двух элементов по возрастанию или убыванию(в зависимости от выбора в программе)
+{
+	if (s == 1)//по возрастанию
+	{
+		if (f.size > l.size)
+		{
+			return 1;
+		}
+		else
+			return 0;
+	}
+	if (s == 0)//по убыванию
+	{
+		if (f.size < l.size)
+		{
+			return 1;
+		}
+		else
+			return 0;
+	}
+};
+//сортировка пузырьком
+void puz_sort(int s, struct dlfail f[], long long size)
+{
+	long i, j;
+	struct dlfail c;
+	for (i = 0; i < size; i++)
+	{
+		for (j = size - 1; j < i; j--)
+		{
+			if (compare(f[j], f[j - 1], s) == 1)//Если сравнение прошло успешно и требуются действия с массивом
+			{
+				c = f[j - 1];
+				f[j - 1] = f[j];
+				f[j] = c;
+			}
+
+		}
+	}
+}
+//
+void vibor(int s, struct dlfail f[], long long size)
+{
+	long i, j, k;
+	struct dlfail c;
+
+}
 main()
 {
 	setlocale(LC_ALL, "Rus");
