aba9d1ab8f7a3b4544f45ecd29b94b69c6fe24c7
Zhurilovaa <Anastasiya.Zhurilova@yandex.ru>
2018-12-15 18:04:32 +0300

    Законченное Task6

--- Zhurilova.a/task6/Source6.cpp
+++ Zhurilova.a/task6/Source6.cpp
@@ -1,47 +1,125 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
+#define _USE_MATH_DEFINES
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 #include <locale.h>
+
 //Функция экспоненты
-long double myexp(long double x, int n, long double eps)
+long double myexp(long double x, int& N, long double eps)
 {
 	int i;
 	long double a=1, y=1;
-	for (i = 2; i <= n; i++)
+	long double zn = exp(x);
+	for (i = 2; i <= N; i++)
 	{
 		a = (a*x) / (i - 1);//Считаем каждый следующий через предыдущий
 		y = y + a;
-		if ()
+		if (fabsl(zn - y) < eps)
+		{
+			N = i-1;
 			return y;
+		}			
 	}
 	return y;
-
 }
-
+//Функция sin
+long double mysin(long double x, int& N, long double eps)
+{
+	int i,j;
+	x = fmod(x, 2 * M_PI);
+	long double a = x, y = x;
+	long double zn = sin(x);
+	for (i = 2,j=0; i <= N; i++,j++)
+	{
+		a = (a*x*x) / ((i + j)*(i + (j + 1)));
+		if (i % 2 == 0)
+			y = y - a;
+		else
+			y = y + a;
+		if (fabsl(zn - y) < eps)
+		{
+			N = i - 1;
+			return y;
+		}
+	}
+	return y;
+}
+//Функция cos
+long double mycos(long double x, int& N, long double eps)
+{
+	int i, j;
+	x = fmod(x, 2 * M_PI);
+	long double a = 1, y = 1;
+	long double zn = cos(x);
+	for (i = 2, j = -1; i <= N; i++, j++)
+	{
+		a = (a*x*x) / ((i + j)*(i + (j + 1)));
+		if (i % 2 == 0)
+			y = y - a;
+		else
+			y = y + a;
+		if (fabsl(zn - y) < eps)
+		{
+			N = i - 1;
+			return y;
+		}
+	}
+	return y;
+}
+//Функция арккосинуса
+long double myacos(long double x, int& N, long double eps)
+{
+	int i;
+	long double a = x, y = (M_PI / 2) - x;
+	long double zn = acos(x);
+	for (i = 2; i <= N; i++)
+	{
+		a = a * (x*x*(2 * i + 1)*(2 * i + 1)) / (2 * (i + 1)*(2 * i + 3));
+		y = y - a;
+		if (fabsl(zn - y) < eps)
+		{
+			N = i - 1;
+			return y;
+		}
+	}
+	return y;
+}
 void main()
 {
 	setlocale(LC_ALL, "Rus");
-	int exit = 0, regim=0, func, flag0 = 0;
-	int N=0, NMax=0;
+	int exit = 0, regim = 0, func, flag0 = 0;
+	int N = 0, i;
 	long double x, eps;
+	long double(*myfunc[4])(long double, int&, long double);
+	myfunc[0] = myexp;
+	myfunc[1] = mysin;
+	myfunc[2] = mycos;
+	myfunc[3] = myacos;
+	double(*ITfunc[4])(double);
+	ITfunc[0] = exp;
+	ITfunc[1] = sin;
+	ITfunc[2] = cos;
+	ITfunc[3] = acos;
+	long double result, myresult;
 	printf("WELCOME!!!\nЖелаете посчитать что-то?\n");
 	while (exit == 0)
 	{
 		do {
-            printf("Выберите режим работы:\n1 - Однократный расчёт функции в заданной точке;\n2 - Серийный эксперимент;\n");
-		    scanf("%d", &regim);
+			printf("Выберите режим работы:\n1 - Однократный расчёт функции в заданной точке;\n2 - Серийный эксперимент;\n");
+			scanf("%d", &regim);
 			if (regim < 1 || regim>2)
 				printf("Вы ввели режим неверно! Попробуйте ещё раз\n");
 		} while (regim < 1 || regim>2);//Ввод режима, пока не будет введено верно
 		if (regim == 1)//Выбран 1 режим
 		{
-			printf("Выберите функцию:\n1 - e^(x);\n2 - sin(x);\n3 - cos(x);\n4 - arccos(x);\n");
+			printf("Выберите функцию:\n0 - e^(x);\n1 - sin(x);\n2 - cos(x);\n3 - arccos(x);\n");
+			flag0 = 0;
 			while (flag0 == 0)//Ввод номера функции
 			{
 				scanf("%d", &func);
 				flag0 = 1;
-				if (func < 1 || func>4)//Если функция введена неверно
+				if (func < 0 || func>3)//Если функция введена неверно
 				{
 					printf("Пожалуйстa введите номер функции, согласно предложенным ранее!\n");
 					flag0 = 0;
@@ -64,16 +142,23 @@ void main()
 					printf("Вы ввели число элементов неверно! Попробуйте ещё раз\n");
 			} while (N < 1 || N>1000);//Ввод числа элементов, пока не будет введено верно
 			//Расчёт
-
+			result = ITfunc[func](x);
+			myresult = myfunc[func](x, N, eps);
+			eps = fabsl(result - myresult);
+			printf("Итог:\nЭталонное значение:                    %lf\n", result);
+			printf("Оценка значения функции:                      %lf\n", myresult);
+			printf("Разницу между оценкой и эталонным значением:  %lf\n", eps);
+			printf("Количество слагаемых, которое было вычислено: %d\n", N);
 		}
-		if(regim==2)//Выбран 2 режим
-		{			
-			printf("Выберите функцию:\n1 - e^(x);\n2 - sin(x);\n3 - cos(x);\n4 - arccos(x);\n");
+		if (regim == 2)//Выбран 2 режим
+		{
+			eps = -1;
+			printf("Выберите функцию:\n0 - e^(x);\n1 - sin(x);\n2 - cos(x);\n3 - arccos(x);\n");
 			while (flag0 == 0)//Ввод номера функции
 			{
 				scanf("%d", &func);
 				flag0 = 1;
-				if (func < 1 || func>4)//Если функция введена неверно
+				if (func < 0 || func>3)//Если функция введена неверно
 				{
 					printf("Пожалуйстa введите номер функции, согласно предложенным ранее!\n");
 					flag0 = 0;
@@ -83,11 +168,20 @@ void main()
 			scanf("%lf", &x);
 			do {
 				printf("Выберите число экспериментов для выполнения расчета\nВНИМАНИЕ! Чичло экспериментов находится в промежутке от 1 до 25\n");
-				scanf("%d", &n);
-				if (n < 1 || n>25)
+				scanf("%d", &N);
+				if (N < 1 || N>25)
 					printf("Вы ввели элементов неверно! Попробуйте ещё раз\n");
-			} while (n < 1 || n>25);//Ввод числа элементов, пока не будет введено верно
-		}	
-
+			} while (N < 1 || N>25);//Ввод числа элементов, пока не будет введено верно
+			result = ITfunc[func](x);
+			printf("N      Эталон         Оценка         Разница\n");
+			printf("-      ------         ------         -------\n");
+			for (i = 1; i <= N; i++)
+			{
+				myresult = myfunc[func](x, i, eps);			
+				printf("%.2d %16.10lf %16.10lf %16.10lf\n", i, result, myresult, fabsl(result - myresult));
+			}
+		}
+		printf("Желаете выйти из программы:0-нет, 1-да\n");
+		scanf("%d", &exit);
 	}
 }
